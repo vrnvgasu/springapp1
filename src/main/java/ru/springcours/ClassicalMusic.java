ab72a2713fa90bb2c499c32f2259860f6c9d58ad
Dmitrii Shitikov <vrnvgasu@yandex.ru>
2022-07-22 16:40:43 +0300

    8. Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы

--- src/main/java/ru/springcours/ClassicalMusic.java
+++ src/main/java/ru/springcours/ClassicalMusic.java
@@ -1,6 +1,25 @@
 package ru.springcours;
 
 public class ClassicalMusic implements Music {
+  // ограничем создание объект через new
+  private ClassicalMusic() {}
+
+  // добавим фабричный метод
+  public static ClassicalMusic getClassicalMusic() {
+    System.out.println("Doing factory-method getClassicalMusic");
+    return new ClassicalMusic();
+  }
+
+  // Будем использовать метод, как init-method. Можем дать любое название
+  public void doMyInit() {
+    System.out.println("Doing ClassicalMusic initialization");
+  }
+
+  // Будем использовать метод, как destroy-method. Можем дать любое название
+  public void doMyDestroy() {
+    System.out.println("Doing ClassicalMusic destruction ");
+  }
+
   @Override
   public String getSong() {
     return "Hungarian Rhapsody ";
--- src/main/java/ru/springcours/TestSpring.java
+++ src/main/java/ru/springcours/TestSpring.java
@@ -9,13 +9,16 @@ public class TestSpring {
             "applicationContext.xml"
     );
 
-    // вызовем два раза бин со скоупом singltone / prototype
-    MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-    MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-    // проверим, что 2 переменные ссылаются на один и тот же объект
-    // singltone - true
-    // prototype - false
-    System.out.println(musicPlayer == secondMusicPlayer);
+    // отработаю методы init в начале (doMyInit()) и destroy (doMyDestroy()) в конце из контекста
+     ClassicalMusic classicalMusic = context.getBean("classicalMusicBean", ClassicalMusic.class);
+
+//    // вызовем два раза бин со скоупом singltone / prototype
+//    MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//    MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//    // проверим, что 2 переменные ссылаются на один и тот же объект
+//    // singltone - true
+//    // prototype - false
+//    System.out.println(musicPlayer == secondMusicPlayer);
 
 
     // обязательно закрывать контекст
